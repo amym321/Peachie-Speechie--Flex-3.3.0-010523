306ce49419eea6bc0ed4e6a98f9b854495f00fea
Amy Mick <amymick847@gmail.com>
2023-01-30 13:42:32 -0600

    correct mobile: Searchanise shifted & Judgeme zoom

--- assets/z__jsProduct.js
+++ assets/z__jsProduct.js
@@ -6972,7 +6972,7 @@ window.PXUTheme.jsProduct = {
         $relatedSlider.flickity({
           freeScrollFriction: 0.075, // friction when free-scrolling
           friction: 0.28, // speed and motion of the slider.
-          resize: true, //snaps slider into place
+          resize: true, // cell size, snaps slider into place
           //resize: false,
           selectedAttraction: 0.025, //speed and motion of the slider.
           //wrapAround: true,
